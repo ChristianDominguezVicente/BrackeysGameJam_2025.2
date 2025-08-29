f45dc2a541e902411cc3047e4383a11864ef5814
victorftome <victorforift@gmail.com>
2025-08-30 00:47:09 +0200

    Cartas al cementerio se resetea

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -211,7 +211,11 @@ public class Player : MonoBehaviour, IHittable
             }
             else
             {
-                Debug.Log("TE QUEDASTE SIN CARTAS");
+                if (cementery.Count > 0)
+                {
+                    ShuffleCementeryIntoDeck();
+                    --i;
+                }
             }
         }
 
@@ -266,7 +270,20 @@ public class Player : MonoBehaviour, IHittable
 
     public void EndTurn()
     {
+        HandleTurnEnd();
         OnTurnEnded?.Invoke();
+    }
+
+    public void HandleTurnEnd()
+    {
+        for (int i = hand.Count - 1; i >= 0; i--)
+        {
+            SendCardToCementery(hand[i]);
+        }
+
+        hand.Clear();
+
+        Draw(handSize);
     }
 
     private void Pause()
@@ -433,15 +450,7 @@ public class Player : MonoBehaviour, IHittable
 
     public void OnCardUsed(GameObject playedCard)
     {
-        CardVisualizer cv = playedCard.GetComponent<CardVisualizer>();
-
-        if (cv != null && cv.card != null)
-        {
-            cementery.Add(cv.card);
-        }
-
-        hand.Remove(playedCard);
-        Destroy(playedCard);
+        SendCardToCementery(playedCard);
 
         if (selectedCardIndex >= hand.Count)
         {
@@ -449,6 +458,36 @@ public class Player : MonoBehaviour, IHittable
         }
 
         UpdateCardPositions();
+    }
+
+    private void SendCardToCementery(GameObject card)
+    {
+        CardVisualizer cv = card.GetComponent<CardVisualizer>();
+
+        if (cv != null && cv.card != null)
+        {
+            cementery.Add(cv.card);
+        }
+
+        hand.Remove(card);
+        Destroy(card);
+    }
+
+    private void ShuffleCementeryIntoDeck()
+    {
+        deck.AddRange(cementery);
+        cementery.Clear();
+
+        for (int i = 0; i < deck.Count; i++)
+        {
+            Card tmp = deck[i];
+            int randomIndex = Random.Range(i, deck.Count);
+
+            deck[i] = deck[randomIndex];
+            deck[randomIndex] = tmp;
+        }
+
+        Debug.Log("Cementerio barajado al mazo");
     }
 
     public void ResetPlayer()
